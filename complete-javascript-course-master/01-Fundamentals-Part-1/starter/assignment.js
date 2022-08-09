7a644d539c57964147707f9ac0a635f9fdc3433a
Ａｓｈｌｅｙｚｚｚｚｚ <u2789011@gmail.com>
2022-08-09 14:22:16 +0800

    updateContent

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignment.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignment.js
@@ -1,7 +1,10 @@
-let country = "Taiwan";
+
+
+/*let country = "Taiwan";
 let contiential = "Asia";
 let population = 23000000
 
 console.log(country);
 console.log(contiential);
 console.log(population);
+*/
--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,6 @@
-let js = 'amazing';
-//if (js === 'amazing') alert ('JavaScript is fun!');
+
+/*let js = 'amazing';
+if (js === 'amazing') alert ('JavaScript is fun!');
       
 console.log(10 + 20 + 30 - 1);
 console.log("Jonas");
@@ -7,4 +8,21 @@ console.log(23)
 
 let firstName = "Bob";
 console.log(firstName);
+*/
 
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "javaScript");
+
+//dynamic type
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+//undefined
+let year;
+console.log(year);
+console.log(typeof year);
