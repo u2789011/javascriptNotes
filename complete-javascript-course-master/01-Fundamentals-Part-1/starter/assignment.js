8ec542caa7ad7caf20baced323f3624448a2c144
Ａｓｈｌｅｙｚｚｚｚｚ <u2789011@gmail.com>
2022-08-09 13:54:44 +0800

    initial

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,7 @@
+let country = "Taiwan";
+let contiential = "Asia";
+let population = 23000000
+
+console.log(country);
+console.log(contiential);
+console.log(population);
---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,10 @@
+let js = 'amazing';
+//if (js === 'amazing') alert ('JavaScript is fun!');
+      
+console.log(10 + 20 + 30 - 1);
+console.log("Jonas");
+console.log(23)
+
+let firstName = "Bob";
+console.log(firstName);
+
