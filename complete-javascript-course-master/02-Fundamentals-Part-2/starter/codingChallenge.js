646d56cad5f83924ddc89b1c0246d77dedd79ebd
Ａｓｈｌｅｙｚｚｚｚｚ <u2789011@gmail.com>
2022-08-11 18:11:41 +0800

    20220811 18:11

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/codingChallenge.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/codingChallenge.js
@@ -1,3 +1,42 @@
 'use strict';
+/*
+const calcAverage = (num1,num2,num3) => (num1 + num2 + num3) /3;
 
-const calcAverage = (num1,num2,num3) => (num1 + num2 + num3) /3
+let scoreDophins = calcAverage(44,23,71);
+let scoreKoalas = calcAverage(65,54,49);
+console.log(scoreDophins,scoreKoalas);
+
+function checkWinner (scoreDophins,scoreKoalas) {
+    if (scoreDophins>=2*scoreKoalas){
+      return(`Dophins wins! (${scoreDophins} vs. ${scoreKoalas})`)
+    }else if (scoreKoalas>=2*scoreDophins){
+      return(`Koalas wins! (${scoreKoalas} vs. ${scoreDophins})`)
+    }else{
+      return (`No teams wins...`)
+    }
+}
+
+const firstResault = checkWinner(scoreDophins,scoreKoalas);
+console.log(firstResault);
+
+scoreDophins = calcAverage(85,54,41);
+scoreKoalas = calcAverage(23,34,27);
+
+const finalResault = checkWinner(scoreDophins,scoreKoalas);
+console.log(finalResault);
+*/
+function calcTip(value){
+  // if (value>=50 && value <= 300){
+  //   const tips = value * 0.15
+  //   return tips;
+  // }else{
+  //   const tips = value * 0.2
+  //   return tips;
+  // }
+  return value >= 50 && value <=300 ? value*0.15 : value*0.2;
+};
+
+const bills = [125,555,44];
+const tips = [calcTip(bills[0]),calcTip(bills[1]),calcTip(bills[2])];
+const total = [bills[0]+tips[0],bills[1]+tips[1],bills[2]+tips[2]];
+console.log(bills,tips,total);
