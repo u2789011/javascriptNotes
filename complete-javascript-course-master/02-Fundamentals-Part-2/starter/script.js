22cca1fd242047efa7924a639be666729ec3ec62
Ａｓｈｌｅｙｚｚｚｚｚ <u2789011@gmail.com>
2022-08-12 11:15:00 +0800

    20220812 11:14

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -104,4 +104,126 @@ const yearsUntilRetirement = function(birthYear, firstname) {
 }
 
 console.log(yearsUntilRetirement(1950,'Jonas'));
-*/
+
+
+//array 可以一次存很多資料 (literal syntax)
+const friends = ['Michael','Steven','Peter'];
+//另一種array寫法(用array function)
+const y = new Array(1991,1984,2008,2020);
+
+//拿到array資料
+console.log(friends[0]);
+//拿到array總共有幾項
+console.log(friends.length);
+//拿到array最後一項
+console.log(friends[friends.length-1]);
+//改array裡的特定項目（不受const限制）
+friends[2] = 'Jay';
+console.log(friends);
+// friends = ['Bob', 'Alice'] 但整個重新換過是不行的
+
+//可以放其他variable，字串，數字，和其他array
+const firstName = 'Jonas';
+const jonas = [firstName, 'Schmedtmann', 2037-1991, friends];
+console.log(jonas);
+console.log(jonas.length);
+
+//array裡的項目可以個別拿出來放進function裡面
+const years = [1990, 1967,2002,2010,2018];
+console.log(calcAge(years[0]));
+console.log(calcAge(years[years.length-1]));
+//會產生值的function也可以放進array裡
+const ages = [calcAge(years[0]),calcAge(years[years.length-1])]
+console.log(ages);
+
+//array method
+
+
+const friends = ['Michael','Steven','Peter'];
+//.push可以把新項目加進array裡，作為最後一個項目
+const newLength = friends.push('Jay');//這功能會return增加後的項目個數
+console.log(friends);
+console.log(newLength);
+//.unshift可以把新項目加進array裡，作為第一個項目
+friends.unshift('John');
+console.log(friends);
+//.pop可以把最後一個項目從array中移除
+const poped = friends.pop();
+console.log(friends);//這功能會顯示被移除的項目
+console.log(poped);
+//.shift可以把第一個項目從array中移除
+friends.shift();
+console.log(friends);
+//indexOf可以顯示項目在array中的位置（第幾個）
+console.log(friends.indexOf('Steven'));
+//如果是不在array中的項目會傳回-1
+console.log(friends.indexOf('Bob')); //-1
+
+
+//include可以顯示該項目有沒有在array裡面（不會自動轉換型別）
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+friends.push(23);
+console.log(friends.includes('23'));//false
+console.log(friends.includes(23));//true
+
+if (friends.includes('Peter')){
+  console.log('You have a friend called Peter!')
+}*/
+
+let x = 10;
+console.log(x);
+
+x=20;
+console.log(x);
+
+const y = 'Hello';
+console.log(y);
+
+const z = '22';
+console.log(z);
+
+console.log(x+z);
+console.log(x+y+z);
+
+function print (x){
+  if (x>18){
+    console.log(x);
+  }else if (x === 18){
+    console.log('they are eqal');
+  }else{
+    console.log('error');
+  }
+}
+
+print(16);
+
+function newPrint(x){
+  switch (x){
+    case '20':
+    console.log('>18');
+    break;
+
+    case '18':
+      console.log('=18');
+    break;
+
+    case '16':
+      console.log('=16');
+      break;
+  }
+}
+
+newPrint('20');
+newPrint('18');
+newPrint('16');
+
+const calcNum = resault => resault -1000 ;
+
+const calcNum2 = calcNum(1001);
+console.log(calcNum2);
+
+const num = [100, 200 ,300];
+console.log(num[1]);
+num.push(400);
+console.log(num[num.length-1]);
