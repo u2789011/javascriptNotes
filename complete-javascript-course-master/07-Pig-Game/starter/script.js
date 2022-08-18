0b7be923b5d9b33c60a8dedff8af1dc9ace93c41
Ａｓｈｌｅｙｚｚｚｚｚ <u2789011@gmail.com>
2022-08-18 18:15:50 +0800

    Hoisting

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -10,16 +10,25 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 const current0El = document.getElementById('current--0');
-const cuttent1El = document.getElementById('current--1');
-//設定初始狀態
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+const current1El = document.getElementById('current--1');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  diceEl.classList.add('hidden');
+};
+init();
 
 const swithchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -60,7 +69,7 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     //滿一百的話結束遊戲
-    if (scores[activePlayer] >= 10) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
       document
         .querySelector(`.player--${activePlayer}`)
@@ -75,13 +84,4 @@ btnHold.addEventListener('click', function () {
   }
 });
 
-btnNew.addEventListener('click', function () {
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-  diceEl.classList.add('hidden');
-
-  scores = [0, 0];
-  currentScore = 0;
-  activePlayer = 0;
-  playing = true;
-});
+btnNew.addEventListener('click', init);
--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+  function printAge() {
+    let output = `${firstName} you are ${age} years old, born in ${birthYear}.`;
+    console.log(output);
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      const firstName = 'Steven'; //local scope優先所以可以重複名字
+      // var millenial = true;
+      const str = `Oh, and you are a millenial, ${firstName}`;
+      console.log(str);
+      function add(a, b) {
+        return a + b;
+      } //strict mode會阻止外面的存取（block scope）
+      // const output = 'New output'; //創造全新的output用重複的名字
+      output = 'New output'; //更改scope chain上一級的output
+    }
+    // console.log(add(2, 3));
+    // console.log(millenial);
+    console.log(output);
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
