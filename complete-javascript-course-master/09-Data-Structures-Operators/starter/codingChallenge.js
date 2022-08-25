b3edc2fcc94a70327790d93e17b75f27cde39724
Ａｓｈｌｅｙｚｚｚｚｚ <u2789011@gmail.com>
2022-08-25 17:55:01 +0800

    20220825

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/codingChallenge.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/codingChallenge.js
@@ -40,24 +40,38 @@ const game = {
     team2: 6.5,
   },
 };
-const properties = Object.values(game.scored);
-for (const name of properties) {
-  console.log(name);
-}
+/*
+// const properties = Object.values(game.scored);
+// for (const name of properties) {
+//   console.log(name);
+// }
+
+for (const [i, players] of game.scored.entries())
+  console.log(`${i + 1}:${players}`);
 
 const oddAverge = Object.values(game.odds);
 // console.log(oddAverge);
 
-for (const odd of oddAverge) {
-  console.log(odd);
-  let sum = 0;
-  sum += odd;
-  console.log(sum);
-  let averge = sum / oddAverge.length;
-  console.log(averge);
-}
+let sum = 0;
+for (const odd of oddAverge) sum += odd;
+// console.log(odd);
+// console.log(sum);
+// console.log(oddAverge.length);
+sum /= oddAverge.length;
+console.log(sum);
 
-/*
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+  console.log(`Odd of ${teamStr}: ${odd}`);
+}
+// So the solution is to loop over the array, and add the array elements as object properties, and then increase the count as we encounter a new occurence of a certain element
+const scorers = {};
+for (const player of game.scored) {
+  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+}
+console.log(scorers);
+
+
 // const player1 = game.players[0];
 // const player2 = game.players[1];
 const [player1, player2] = game.players;
