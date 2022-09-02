4f69f3fba474770e7beb42db309d742e59693f8d
Ａｓｈｌｅｙｚｚｚｚｚ <u2789011@gmail.com>
2022-09-02 17:09:05 +0800

    20220902

---
+++ complete-javascript-course-master/10-Functions/starter/codingChallenge.js
@@ -0,0 +1,45 @@
+'use strict';
+/*
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3:C++'],
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    const ans = prompt(
+      `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+    );
+    console.log(ans);
+    switch (ans) {
+      case '0':
+        this.answers[0]++;
+        console.log(this.answers[0]);
+        break;
+      case '1':
+        this.answers[1]++;
+        console.log(this.answers[1]);
+        break;
+      case '2':
+        this.answers[2]++;
+        console.log(this.answers[2]);
+        break;
+      case '3':
+        this.answers[3]++;
+        console.log(this.answers[3]);
+        break;
+    }
+    displayResults(ans);
+  },
+  displayResults(type) {
+    console.log(typeOf(type));
+    const typeOf = typeOf(type);
+    if (typeOf === 'array') {
+      console.log(type);
+    } else {
+      console.log(`Poll results are:${type}`);
+    }
+  },
+};
+console.log(poll);
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));*/
--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -1,3 +1,199 @@
 'use strict';
+/*
+const bookings = [];
+const createBooking = function (
+  flightNum,
+  numPasenger = 1,
+  price = 199 * numPasenger
+) {
+  //ES5
+  // numPasenger = numPasenger || 1;
+  // price = price || 199;
+  const booking = {
+    flightNum,
+    numPasenger,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+createBooking('LH230', 2, 800);
+createBooking('LH456');
+createBooking('LH123', undefined, 1000);
+const flight = 'LH330';
+const jonas = {
+  name: 'jonas',
+  passport: 3456209815,
+};
 
-const createBooking = function (flightNum, numPasenger, price) {};
+const checkIn = function (flightNum, passnger) {
+  flightNum = 'LH999';
+  passnger.name = 'Mr. ' + passnger.name;
+  if (passnger.passport === 3456209815) {
+    alert('Check in!');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+checkIn(flight, jonas);
+console.log(flight); //LH330 是primitive所以是複製新的variable不會改到原本的
+// const flightNum = flight; //複製一個新的variable
+console.log(jonas); //Object { name: "Mr. jonas", passport: 3456209815 }
+//是referfance所以是複製原本的地址會改到原本的
+// const passnger = jonas; //複製地址存成另一個variable
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000000);
+};
+newPassport(jonas);
+checkIn(flight, jonas);
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('👋');
+};
+document.body.addEventListener('click', high5);
+['Jonas', 'Martha', 'Adam'].forEach(high5);
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+greeterHey('Steven');
+
+greet('Hello')('Jonas');
+
+const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+
+greetArr('Hi')('Jonas');
+
+
+// The call and apply Methods
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  // book: function() {}
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, 'Jonas Schmedtmann');
+lufthansa.book(635, 'John Smith');
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+// Does NOT work
+// book(23, 'Sarah Williams');
+
+// Call method
+book.call(eurowings, 23, 'Sarah Williams');
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'Mary Cooper');
+console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+book.call(swiss, 583, 'Mary Cooper');
+
+// Apply method
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+book.call(swiss, ...flightData);
+
+// The bind Method
+book.call(lufthansa, 23, 'Sarah Williams');
+
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(23, 'Steven Williams');
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Jonas Schmedtmann');
+bookEW23('Martha Cooper');
+
+// With Event Listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+
+  this.planes++;
+  console.log(this.planes);
+};
+// lufthansa.buyPlane();
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// addVAT = value => value + value * 0.23;
+
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));
+const a = 123;
+const arr = [100, 200, 300];
+
+function test(a, b) {
+  a = a + 100;
+  b[0] = b[0] + 100;
+  // console.log(a, b);
+}
+test(a, arr);
+console.log(a, arr);*/
