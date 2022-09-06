0856692995744bc884a889bc37cba24db04bc636
Ａｓｈｌｅｙｚｚｚｚｚ <u2789011@gmail.com>
2022-09-06 12:21:11 +0800

    Update script.js

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -73,4 +73,47 @@ const currencies = new Map([
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
+for (const movement of movements) {
+}
 /////////////////////////////////////////////////
+/*
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+//slice
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(1, -2));
+console.log(arr.slice());
+console.log([...arr]);
+console.log(arr);
+
+//splice
+console.log(arr.splice(2));
+arr.splice(-1);
+console.log(arr);
+arr.splice(1, 2);
+console.log(arr);
+
+//reverse
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+//concat
+const letters = arr.concat(arr2);
+console.log(...letters);
+console.log(arr);
+console.log(...arr, ...arr2);
+
+//join
+console.log(arr.join('-'));
+
+//at
+console.log(arr[0]);
+console.log(arr.at(0));
+
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+
+console.log(arr.at(-1));*/
