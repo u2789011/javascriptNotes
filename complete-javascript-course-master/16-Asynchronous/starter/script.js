f2c849f2e1019b12ea7dfaefbda40d404c5781c9
Ａｓｈｌｅｙｚｚｚｚｚ <u2789011@gmail.com>
2022-09-08 18:43:05 +0800

    20220908

--- complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -4,3 +4,30 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+const request = new XMLHttpRequest();
+request.open('Get', 'https://restcountries.com/v3.1/name/portugal');
+request.send();
+
+request.addEventListener('load', function () {
+  const data = JSON.parse(this.responseText);
+  console.log(data);
+
+  const html = `
+  <article class="country">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>P${
+              (+data.region / 100000).toFixed
+            }</p>
+            <p class="country__row"><span>🗣️</span>${data.languages.por}</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies.EUR.name
+            }</p>
+          </div>
+        </article>
+  `;
+  countriesContainer.insertAdjacentElement('beforeend', html);
+  countriesContainer.styles.opasity = 1;
+});
