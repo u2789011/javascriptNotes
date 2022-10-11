4f004ffd11020f55fe16c0b36fd186bee394decb
Ａｓｈｌｅｙｚｚｚｚｚ <u2789011@gmail.com>
2022-10-11 17:58:25 +0800

    20221011

--- complete-javascript-course-master/16-Asynchronous/starter/codingChallenge.js
+++ complete-javascript-course-master/16-Asynchronous/starter/codingChallenge.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 const whereAmI = function (lat, lng) {
   fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
     .then(res => res.json())
@@ -8,3 +8,74 @@ const whereAmI = function (lat, lng) {
     });
 };
 whereAmI(52.508, 13.381);
+
+// const imagesContainer = document.querySelector('.images');
+
+// const createImage = function (imgPath) {
+//   return new Promise(function (resolve, reject) {
+//     const img = document.createElement('img');
+//     img.src = imgPath;
+//     img.addEventListener('load', function () {
+//       imagesContainer.append(img);
+//       resolve(img);
+//     });
+//     img.addEventListener('error', function () {
+//       reject(new Error('Image not found'));
+//     });
+//   });
+// };
+// let currentImage;
+// createImage('img/img-1.jpg')
+//   .then(img => console.log('Image 1 loaded'))
+//   .then(() => {
+//     currentImage.styles.display = 'none';
+//     return createImage('img/img-2.jpg');
+//   })
+//   .then(img => {
+//     currentImage = img;
+//     console.log('Image 2 loaded');
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImage.styles.display = 'none';
+//   })
+//   .catch(err => console.error(err));
+const imgContainer = document.querySelector('.images');
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  });
+};
+
+let currentImg;
+
+createImage('img/img-1.jpg')
+  .then(img => {
+    currentImg = img;
+    console.log('Image 1 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    console.log('Image 2 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+  })
+  .catch(err => console.error(err));*/
--- complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -302,12 +302,67 @@ Promise.any([
   Promise.resolve('Another success'),
 ])
   .then(res => console.log(res))
-  .catch(err => console.error(err));*/
+  .catch(err => console.error(err));
 
 const lotteryPromise = new Promise(function (resolve, reject) {
-  if (Math.ramdom() >= 0.5) {
-    resolve('You WIN 💰');
-  } else {
-    reject(new Error('You lost your money 💩'));
-  }
+  console.log('Lottery draw is happening 🔮');
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('You WIN 💰');
+    } else {
+      reject(new Error('You lost your money 💩'));
+    }
+  }, 2000);
 });
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.log(err));
+
+// Promisifying setTimeout
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+wait(2)
+  .then(() => {
+    console.log('I waited for 2 seconds');
+    return wait(1);
+  })
+  .then(() => console.log('I waited for 1 second'));
+
+wait(1)
+  .then(() => {
+    console.log('I waited for 1 second');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('I waited for 2 second');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('I waited for 3 second');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('I waited for 4 second');
+    return wait(1);
+  });
+
+// setTimeout(() => {
+//   console.log('1 second passed');
+//   setTimeout(() => {
+//     console.log('2 seconds passed');
+//     setTimeout(() => {
+//       console.log('3 second passed');
+//       setTimeout(() => {
+//         console.log('4 second passed');
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+
+Promise.resolve('abc').then(x => console.log(x));
+Promise.reject(new Error('!!!')).catch(x => console.log(x));
+
+*/
